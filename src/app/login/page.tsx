22f16510d339a874111a5f70a92b3788fce00b32
chirag sonar <chiragsonar1718@gmail.com>
2023-12-15 04:11:33 +0530

    chore: contains login component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./components/Login";
+
+const page = () => {
+  return (
+    <div className="">
+      <Login />
+    </div>
+  );
+};
+
+export default page;
